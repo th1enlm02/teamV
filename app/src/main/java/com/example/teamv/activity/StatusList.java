19541702e348980d8bd5632c4d3675ebb4d62af3
th1enlm02 <21521460@gm.uit.edu.vn>
2023-12-20 23:42:15 +0700

    edited status list layout

--- app/src/main/java/com/example/teamv/activity/StatusList.java
+++ app/src/main/java/com/example/teamv/activity/StatusList.java
@@ -3,11 +3,14 @@ package com.example.teamv.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.teamv.R;
 import com.example.teamv.object.Board;
 
 public class StatusList extends AppCompatActivity {
+
+    private TextView tvTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +18,21 @@ public class StatusList extends AppCompatActivity {
         setContentView(R.layout.activity_status_list);
 
         // Lấy thông tin board từ main activity
-        getBoardInfoFromMainActivity();
+        Board board = getBoardInfoFromMainActivity();
+
+        // findviewbyids
+        findViewByIds();
+
+        // set title
+        tvTitle.setText(board.getName());
 
     }
-    private void getBoardInfoFromMainActivity() {
+    private Board getBoardInfoFromMainActivity() {
         Bundle bundle = getIntent().getExtras();
-        if (bundle == null)
-            return;
         Board board = (Board) bundle.get("object_board");
+        return board;
+    }
+    private void findViewByIds() {
+        tvTitle = (TextView) findViewById(R.id.tv_title_board_name);
     }
 }
