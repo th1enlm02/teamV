12333ef6e67463ad91c4017b24db2a26bfbaa03c
ThienNoob <102866314+ThienNoob@users.noreply.github.com>
2024-01-12 21:13:47 +0700

    adapter calendar, lum la về lịch

--- app/src/main/java/com/example/teamv/fragment/NotificationsFragment.java
+++ app/src/main/java/com/example/teamv/fragment/NotificationsFragment.java
@@ -3,19 +3,36 @@ package com.example.teamv.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.teamv.R;
+import com.example.teamv.adapter.CalendarAdapter;
 
-public class NotificationsFragment extends Fragment {
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
+public class NotificationsFragment extends Fragment  {
+
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+
+
+        view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        return view;
     }
+
+
+
 }
